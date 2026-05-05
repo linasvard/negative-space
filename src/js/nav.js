b49189b64e9d589ed56d11ff5d968a65970b74ca
linasvard <linajsvard@hotmail.com>
2026-05-05 15:39:25 +0200

    fix: implement js on dropdown menu on tablet and mobile

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,6 +1,18 @@
 export function initNav() {
   
   // nav-logik här
+const toggleBtn = document.getElementById("toggleHamburgerMenu");
+const closeBtn = document.getElementById("closeMobileMenu");
+const dropdown = document.querySelector(".mobileTabletDropdown");
 
-  
+toggleBtn.addEventListener("click", () => {
+  dropdown.classList.remove("hidden");
+  toggleBtn.classList.add("hidden");
+});
+
+closeBtn.addEventListener("click", () => {
+  dropdown.classList.add("hidden");
+  toggleBtn.classList.remove("hidden");
+});
+
 }
