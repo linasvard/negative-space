b5c440ea24b5db1fb3504c3d13cdefd4ce34d035
linasvard <linajsvard@hotmail.com>
2026-05-06 15:21:35 +0200

    style: add animation to menu dropdown tablet and mobile

--- src/js/nav.js
+++ src/js/nav.js
@@ -6,13 +6,15 @@ const closeBtn = document.getElementById("closeMobileMenu");
 const dropdown = document.querySelector(".mobileTabletDropdown");
 
 toggleBtn.addEventListener("click", () => {
-  dropdown.classList.remove("hidden");
+  dropdown.classList.add("open");
+  dropdown.setAttribute("aria-hidden", "false");
   toggleBtn.classList.add("hidden");
   closeBtn.classList.remove("hidden");
 });
 
 closeBtn.addEventListener("click", () => {
-  dropdown.classList.add("hidden");
+  dropdown.classList.remove("open");
+  dropdown.setAttribute("aria-hidden", "true");
   closeBtn.classList.add("hidden");
   toggleBtn.classList.remove("hidden");
 });
