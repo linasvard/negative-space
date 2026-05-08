4d7b7be10e1eb8895ce891d319976f99b1d37309
linasvard <linajsvard@hotmail.com>
2026-05-08 12:20:19 +0200

    style: change position to div element in HTML and fix button so i functions

--- src/js/preorder.js
+++ src/js/preorder.js
@@ -1,3 +1,18 @@
 export function initPreorder() {
-  // preorder-logik här
+const preorderBtn = document.querySelector('#preorderBtn');
+const preorderPopup = document.querySelector('#preorderPopupWrapper');
+const closePreorderBtn = document.querySelector('#closePreorderInFormBtn, #closePreorderIconBtn');
+
+preorderBtn.addEventListener('click', () => {
+  preorderPopup.classList.add('open');
+  preorderPopup.setAttribute('aria-hidden', 'false');
+
+})
+
+closePreorderBtn.addEventListener('click', () => {
+  preorderPopup.classList.remove('open');
+  preorderPopup.setAttribute('aria-hidden', 'true');  
+
+})
+
 }
